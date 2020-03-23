2ccba8a9461a4a787b13ab473c989ca7ef5b84cc
johnlapp <lohnjapp@gmail.com>
2020-03-23 11:52:02 -0400

    Added basic enemies with simple chase scripts

---
+++ 4770GroupProject-master/Assets/EnemyPacingMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPacingMovement : MonoBehaviour
+{
+    public Transform player;
+    int moveSpeed = 4;
+    int minDist = 3;
+    int maxDist = 10;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.LookAt(player);
+
+        if (Vector3.Distance(transform.position, player.position) >= minDist) {
+            transform.position += transform.forward * moveSpeed * Time.deltaTime;
+
+            if (Vector3.Distance(transform.position, player.position) <= maxDist) {
+                // Call some type of attack logic 
+            }
+        }
+    }
+}
